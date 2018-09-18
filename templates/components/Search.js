b99fcbf4aa4af3f1f26e2174c8fb498fe1da553e
estrattonbailey <ericstrattonbailey@gmail.com>
2018-09-18 17:23:10 -0400

    Fix z-index

--- templates/components/Search.js
+++ templates/components/Search.js
@@ -6,9 +6,9 @@ module.exports = function Search (props, children) {
 
         <div class='outer'>
           <div class='header__container container'>
-            <div class='header__bar rel z0'>
-              <span class='block header__bar__bg abs fill z9'></span>
-              <form data-component='search' class='search z1 rel s4'>
+            <div class='header__bar rel'>
+              <span class='block header__bar__bg abs fill z0'></span>
+              <form data-component='search' class='search rel s4 z1'>
                 <input class='block x' type='search' name='search' placeholder='Type something' required />
                 <input type='submit' value='Search' />
                 <button type='button' class='abs top right bottom mya'>
